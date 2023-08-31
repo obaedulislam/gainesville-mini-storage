f61083140971fafad86ae7d8a35b92f775d8050e
obaedul islam <obaedul@redlimesolutions.com>
2023-08-31 14:19:00 +0600

    Pay_online page done

---
+++ pay-online.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <!--|| Bundle File Start ||-->
+  <?php include "bundle.php" ?>
+  <!--|| Bundle File End ||-->
+
+
+<body>
+  <!--|| Header Section Start ||-->
+  <?php include "header.php" ?>
+  <!--|| Header Section End ||-->
+
+
+  <!--|| Main Section Start ||-->
+  <main>
+    <!--|| Banner Section Start ||-->
+      <section class="banner-section pay-online-banner">
+        <div class="banner-container text-center ">
+          <div class="row banner-content">
+              <h2>Pay Online | My Account</h2>           
+          </div>
+        </div>
+      </section>
+    <!--|| Banner Section End ||-->
+
+    
+    <!--|| Location Section Start ||-->
+    <section class="pay-online-section first-padding overflow-hidden">
+      <div class="container">
+        <div class="row">
+            <p>We offer many climate-controlled storage units with interior access for extra security and cleanliness. There is also the option of drive-up access, which allows you to park directly in front of the unit and enter from the outside. Our units have good lighting, which is an essential feature in any storage facility that wants to make the customer experience as easy as possible.</p>
+        </div>
+        <!--Page small description End-->
+ 
+      </div>
+    </section>
+    <!--|| Location Section End ||-->
+
+    
+    <!--|| Pay Online Item Section Start ||-->
+    <section class="online-item-section first-padding unit-filter-section">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-md-12 col-12">
+            <div class="category-details">
+                <div class="single-unit heading">
+                  <div class="row">
+                    <div class="col-md-3 col-6 head-size">
+                      <p>Facility</p>
+                    </div>
+                    <div class="col-md-2 head-unit">
+                      <p class="common-pl">Address</p>
+                    </div>
+                     <div class="col-md-2 col-6 head-unit">
+                       <p class="common-pl">City </p>
+                    </div>
+                     <div class="col-md-1 col-6 head-unit">
+                       <p class="common-pl">State </p>
+                    </div>
+                     <div class="col-md-4 col-6 ">
+                       <p class="common-pl">Phone </p>
+                    </div>
+                  </div>
+                </div>
+                <!--Single Unit Heading End -->
+
+                <div class="single-unit">
+                  <div class="row wrapper">
+                    <div class="col-md-3 col-6 store-size">
+                        <p>Gainesville Mini Storage</p>                    
+                    </div>
+                    <div class="col-md-2 store-address">
+                        <p class="fw-normal common-pl">2537 FM 51</p>                
+                    </div>
+                     <div class="col-md-2 col-4 store-city">
+                        <p class="fw-normal common-pl">Gainesville</p>             
+                    </div>
+                     <div class="col-md-1 col-4 store-state">
+                        <p class="fw-normal common-pl">TX</p>             
+                    </div>
+                    <div class="col-md-4 col-4 call-btn">
+                        <p class="common-pl">(940) 400-2297</p>
+                        <a href="#" class="button primary-button ms-5"><i class="icon-fluent-arrow-right"></i>Pay</a>
+                    </div>
+                  </div>
+                </div>
+                <!--Single Unit Heading End -->
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--|| Pay Online Item Section End ||-->
+
+  </main>
+  <!--|| Main Section End ||-->
+
+   <!--|| Footer Section Start ||-->
+  <?php include "footer.php" ?>
+  <!--|| Footer Section End ||-->
+
+</body>
+</html>
