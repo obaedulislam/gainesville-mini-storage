6930f28095ecb540bcd45a333cc8870b288d50b5
obaedul islam <obaedul@redlimesolutions.com>
2023-09-05 16:55:24 +0600

    header half done

--- header.php
+++ header.php
@@ -1,10 +1,28 @@
+  <!DOCTYPE html>
+<html lang="en">
+
+  <!--|| Bundle File Start ||-->
+  <?php include "bundle.php" ?>
+  <!--|| Bundle File End ||-->
+<script>
+    $(document).ready(function () {
+      $("#toggleMenuMobile").click(function () {
+        $("#toggleMenuItem").toggleClass("active");
+      });
+    });
+</script>
+
+<body>
+
+  
   <header>
     <div class="container ">
       <div class="row overflow-hidden">
-        <div class="col-sm-12 col-md-3 col-lg-3">
-          <a class="logo" href="index.php"><img src="images/logo.svg" alt = "Gainesville Mini Storage Logo"></a>
+        <div class="col-4 col-sm-4  col-md-3 col-lg-3 logo-part">
+          <a class="logo " href="index.php"><img src="images/logo.svg" alt = "Gainesville Mini Storage Logo"></a>
         </div>
-        <div class="col-sm-12 col-md-9 col-lg-9 right-header">
+
+        <div class="col-sm-12 col-md-9 col-lg-9 right-header menu-md">
           <ul class="navigation">
             <li><a href="unit-prices.php">Unit Prices</a></li>
             <li><a href="size-guide.php">Size Guide</a></li>
@@ -14,6 +32,27 @@
             <li><a href="pay-online.php">Pay Online</a></li>
           </ul>
         </div>
+
+        <div class="col-8 col-sm-8  menu-text menu-mob">
+          
+          <ul class="navigation-menu ul-list">
+             <li><a id="toggleMenuMobile" class="hamburger-menu"><span><img src="images/header/hamburger.svg" alt=""></span></a>
+                    <ul id="toggleMenuItem" class="ul-list d-flex align-items-center">
+                        <div>
+                          <li><a href="unit-prices.php">Unit Prices</a></li>
+                          <li><a href="size-guide.php">Size Guide</a></li>
+                          <li><a href="nearby.php">Nearby</a></li>
+                          <li><a href="faqs.php">FAQ</a></li>
+                          <li><a href="contact.php">Contact Us</a></li>
+                          <li><a href="pay-online.php">Pay Online</a></li>  
+                        </div>
+                    </ul>
+          
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </header>
+
+</body>
--- pay-online.php
+++ pay-online.php
@@ -47,19 +47,19 @@
             <div class="category-details">
                 <div class="single-unit heading">
                   <div class="row">
-                    <div class="col-md-3 col-6 head-size">
-                      <p>Facility</p>
+                    <div class="col-md-3 col-12 head-size">
+                      <p class="facility">Facility</p>
                     </div>
-                    <div class="col-md-2 head-unit">
+                    <div class="col-md-2  head-unit hide-sm">
                       <p class="common-pl">Address</p>
                     </div>
-                     <div class="col-md-2 col-6 head-unit">
+                     <div class="col-md-2 col-6 head-unit hide-sm">
                        <p class="common-pl">City </p>
                     </div>
-                     <div class="col-md-1 col-6 head-unit">
+                     <div class="col-md-1 col-6 head-unit hide-sm">
                        <p class="common-pl">State </p>
                     </div>
-                     <div class="col-md-4 col-6 ">
+                     <div class="col-md-4 col-6 hide-sm">
                        <p class="common-pl">Phone </p>
                     </div>
                   </div>
@@ -68,24 +68,24 @@
 
                 <div class="single-unit last-unit">
                   <div class="row wrapper">
-                    <div class="col-md-3 col-6 store-size">
-                        <p>Gainesville Mini Storage</p>                    
+                    <div class="col-md-3 col-12 store-size">
+                        <p class="mini-storage">Gainesville Mini Storage</p>                    
                     </div>
-                    <div class="col-md-2 store-address">
+                    <div class="col-md-2 col-12 store-address">
                         <p class="fw-normal common-pl">2537 FM 51</p>                
                     </div>
-                     <div class="col-md-2 col-4 store-city">
+                     <div class="col-md-2 col-12 store-city">
                         <p class="fw-normal common-pl">Gainesville</p>             
                     </div>
-                     <div class="col-md-1 col-4 store-state">
+                     <div class="col-md-1 col-12 store-state">
                         <p class="fw-normal common-pl">TX</p>             
                     </div>
-                    <div class="col-md-4 col-4 call-btn">
+                    <div class="col-md-4 col-12 call-btn">
                         <p class="common-pl">(940) 400-2297</p>
-                        <a href="#" class="button primary-button ms-5"><i class="icon-fluent-arrow-right"></i>Pay</a>
+                        <a href="#" class="button primary-button ms-md-5 ms-0"><i class="icon-fluent-arrow-right"></i>Pay</a>
                     </div>
                   </div>
-                </div>
+                </div> 
                 <!--Single Unit Heading End -->
 
             </div>
